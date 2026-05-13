0c8cc3c1f6727cce2340cb296a0fc8a57f6ae6cc
Parth Kale <parthkale416@gmail.com>
2026-05-13 21:34:05 +0530

    Add files via upload

---
+++ arethmatic.py
@@ -0,0 +1,15 @@
+a=10
+b=6
+
+print("the addtion is ",a+b)
+print("the substraction is ",a-b)
+print("the multiplication is ",a*b)
+print("the division is ",a/b)
+print("the remainder is ",a//b)
+# // modulo hai
+print("the power is ",a**b)
+
+a = 10
+a += 2
+
+print(a)
