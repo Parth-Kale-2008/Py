2b591ce172910f11625fd7c49e3ad9d9e490e41b
Parth Kale <parthkale416@gmail.com>
2026-05-18 23:57:23 +0530

    Add files via upload

---
+++ Typecasting.py
@@ -0,0 +1,17 @@
+# Convert string to integer
+num_str = "10"
+num_int = int(num_str)
+print(num_int) 
+print(type(num_int)) 
+
+# Convert integer to string
+num = 25
+num_str = str(num)
+print(num_str)
+print(type(num_str))  
+
+# Convert float to integer
+pi = 3.14
+pi_int = int(pi)
+print(pi_int)
+print(type(pi_int))  
