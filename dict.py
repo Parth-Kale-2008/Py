4e00620798dbc8002d797c83e0c70d4fcc3311d1
Parth Kale <parthkale416@gmail.com>
2026-06-09 21:10:02 +0530

    Add files via upload

---
+++ dict.py
@@ -0,0 +1,14 @@
+students = {"name":"Parth","age":19,"city":"pune"}
+
+print(students["name"])
+students["age"]
+
+print(students.keys())
+print(students.values())
+students.pop("age")
+students["city"] = "patna"
+print(students)
+
+#numbers = [5,2,9,1,7]
+#print(numbers.sort())
+#print(numbers.append(10))
